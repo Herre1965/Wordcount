bde411ea94fa8ce14458a4e03d0697273a045768
herre <herre.r.scherpenzeel@ing.com>
2022-12-22 09:52:27 +0100

    wordcount versie 2212

--- src/main/java/Nl/herre/java/Main.java
+++ src/main/java/Nl/herre/java/Main.java
@@ -6,7 +6,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         WordFrequencyAnalyzer word = new WordFrequencyAnalyzerImpl();
+        //opdracht 1.
         word.calculateHighestFrequency("the sunshine shines over the lake");
+        // opdracht 2.
+        String  text = "the sunshine shines again and again and again over the big lake";
+        String woord = "   again ";
+        word.calculateFrequencyForWord(text,woord);
+        System.out.println("het woord" + woord + "komt " + (word.calculateFrequencyForWord(text,woord)) + " voor");
+       //opdracht 3.
 
-          }
+        String tekst = "the sunshine shines over and over the lake again";
+        List<WordFrequency> wordFrequencies = word.calculateMostFrequentNWords(tekst, 3);
+        for (WordFrequency wordFrequency : wordFrequencies) {
+            System.out.println(wordFrequency.getWord()+ ": "+ wordFrequency.getFrequency());
+
+
+        }
+
+    }
+
 }
--- src/main/java/Nl/herre/java/WordFrequencyAnalyzerImpl.java
+++ src/main/java/Nl/herre/java/WordFrequencyAnalyzerImpl.java
@@ -1,15 +1,12 @@
 package Nl.herre.java;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.ArrayList;
+import java.util.*;
 
 public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer {
     /**
      * CalculateHighestFrequency should return the highest frequency
      * in the text (severalwords might have this frequency)
+     *
      * @param text ingevoerd door gebruiker
      * @return highest frequency
      */
@@ -24,44 +21,70 @@ public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer {
         for (String s : woorden) {
             // als woord al bestaat ophogen ipv op 1 zetten
 //        teller.put(s, 1);
-       if (teller.keySet().contains(s))
-           teller.put(s,teller.get(s) + 1);
-       else
-           teller.put(s,1);
+            if (teller.keySet().contains(s))
+                teller.put(s, teller.get(s) + 1);
+            else
+                teller.put(s, 1);
 
-              }
+        }
 // Hoogste woord aantal terug geven nadat aantallen bepaald zijn.
 // woordje opvangen in variabele en maximum aantal ook
         String meeste = "";
         int maximum = 0;
-        for (Map.Entry<String,Integer> entry : teller.entrySet())
-        {
+        for (Map.Entry<String, Integer> entry : teller.entrySet()) {
             String sleutel = entry.getKey();
             Integer tel = entry.getValue();
-            if (tel > maximum)
-            {
+            if (tel > maximum) {
                 maximum = tel;
                 meeste = sleutel;
-             }
-            else if (tel== maximum ) {
+            } else if (tel == maximum) {
                 if (sleutel.length() < meeste.length())
                     meeste = sleutel;
             }
         }
-     System.out.println("Het meest voorkomende woord: " +meeste);
-        System.out.println("Het woord komt: " + maximum +  " keer voor");
-    return 0;
+        System.out.println("Het meest voorkomende woord: " + meeste);
+        System.out.println("Het woord komt: " + maximum + " keer voor");
+        return 0;
     }
 
 
     @Override
-    public int calculateFrequencyForWord(String text, String word) {
+    public int calculateFrequencyForWord(String text, String woord) {
+/**
+ * CalculateFrequencyForWord should return the frequency
+ * of the specified word in the text
+ * @param text en specifiek woord ingevoerd door gebruiker
+ * @return aantal keren dat meegegeven woord voorkomt in de tekst
+ */
+        // Ik krijg een  tekst binnen. Ik wil het aantal keren tellen dat meegegeven woord voorkomt.
+        // Ipv hele tekst ieder woord opslaan.
+        // String 'woorden' splitsen door middel van split.
+        // als woord op index nr gelijk is aan woord dat is ingegeven dan teller ophogen voor het ingegeven woord.
+        // trim haalt whitespaces weg.
+        String trimw = woord.trim();
 
-        return 0;
-   }
+        String woorden[] = text.split(" ");
+        int tel = 0;
+        for (int i = 0; i < woorden.length; i++) {
+            if (trimw.equalsIgnoreCase(woorden[i]))
+                tel++;
+        }
+        return tel;
+    }
 
     @Override
     public List<WordFrequency> calculateMostFrequentNWords(String text, int n) {
+
+        String lijst[] = text.split(" ");
+
+//        for (String index: lijst)
+//        { System.out.println(index + ": " + Collections.frequency(List.of(lijst),index));
+//        }
+
+// calculateMostFrequentWords krijgt een lijst met WordFrequency objecten terug.
+// WordFrequency is een interface, geen object. Dit moeten we nog maken.  ... implements WordFrequency
+// WordFrequency heeft een frequency en een woord. Die moet je vullen per lijst element.
+
         return null;
     }
 }
